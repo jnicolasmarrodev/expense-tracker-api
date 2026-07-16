44190fe82fe1a6e9ce6ec54d43b7d9d140301a47
Juan Nicolás Marroquín <jnicolas.marro.dev@gmail.com>
2026-07-16 16:28:02 -0500

    Create User Entity

---
+++ src/main/java/com/nico/expensetracker/entity/User.java
@@ -0,0 +1,33 @@
+package com.nico.expensetracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
